923b1caf2c49a11cf1d738e3403121cc31222cc0
Shayban Nasif <45189438+Shayban-Nasif@users.noreply.github.com>
2026-02-18 15:15:17 +0900

    Create index.js

---
+++ js/services/index.js
@@ -0,0 +1,6 @@
+// js/services/index.js - Central export of all services
+export { TripService } from './trip-service.js';
+export { StudentService } from './student-service.js';
+export { HistoryService } from './history-service.js';
+export { NotificationService } from './notification-service.js';
+export { UserService } from './user-service.js';
